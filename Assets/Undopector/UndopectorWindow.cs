25d4ec54532d0b8d8ca01a572d561361213b7277
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-10-20 12:40:26 +0900

    :new: add Project

---
+++ Assets/Undopector/UndopectorWindow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class UndopectorWindow : EditorWindow
+{
+    [MenuItem("Window/Undopector/Open")]
+    private static void Open()
+    {
+        var window = CreateInstance<UndopectorWindow>();
+        window.Show();
+    }
+}
