0c948c9738c739e1e5cbec12b8c0b36e3036030a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-04-04 02:20:05 +0900

    Preferences用のスクリプト追加

---
+++ Assets/Undopector/Editor/Scripts/UndopectorPreferences.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace HK.Undopector
+{
+    public static class UndopectorPreferences
+    {
+        private static int historyLimit;
+
+        [PreferenceItem("Undopector")]
+        public static void OnPreferenceGUI()
+        {
+
+        }
+    }
+}
